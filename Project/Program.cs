241ddbc85f34b1f217b8e92d9f4532ccb72a6b93
NikitaZharikov <nikitkazharikov@gmail.com>
2023-02-22 19:53:30 +0300

    add void CreateSecondArray

--- Project/Program.cs
+++ Project/Program.cs
@@ -2,3 +2,16 @@
     { "123", "23", "hello", "world", "Denmark", "1234", "Russia", "-2" };
 
 string[] array2 = new string[array1.Length];
+
+void CreateSecondArray(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+        if (array1[i].Length <= 3)
+        {
+            array2[count] = array1[i];
+            count++;
+        }
+    }
+}
