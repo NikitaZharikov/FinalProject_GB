e19d4a01f3b39a0a31062bfe4632a05280e08f70
NikitaZharikov <nikitkazharikov@gmail.com>
2023-02-22 19:53:57 +0300

    add void PrintArray

--- Project/Program.cs
+++ Project/Program.cs
@@ -15,3 +15,12 @@ void CreateSecondArray(string[] array1, string[] array2)
         }
     }
 }
+
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + "\t");
+    }
+    Console.WriteLine();
+}
